781b2263ee8d9b4dc3f154f75a1f1960cd1555fd
Kushim Jiang <kushim_jiang@outlook.com>
2022-06-17 09:53:31 +0800

    Correct urugu and nirugu, 2022-06-17.

--- src/build.py
+++ src/build.py
@@ -11,7 +11,6 @@ otl_path = otl_dir / "main.fea"
 
 
 def main():
-    # inlined_otl_path = otl_dir / "stateless.fea"
     inlined_otl_path = otl_dir / "utn-font.ufo" / "features.fea"
     include_statement_pattern = re.compile(r"include\((.+)\);\n")
     with inlined_otl_path.open("w") as inlined_otl:
@@ -22,6 +21,15 @@ def main():
                     inlined_otl.write(included_otl_path.read_text())
                 else:
                     inlined_otl.write(line)
+
+    # dist
+    ufo = Font.open(directory / "utn-font.ufo")
+    with inlined_otl_path.open("a") as inlined_otl:
+        inlined_otl.write("\n\n")
+        inlined_otl.write("feature dist {\n")
+        for glyph_name in ["nirugu", "fvs1", "fvs2", "fvs3", "fvs4"]:
+            inlined_otl.write("\tpos " + glyph_name + " " + str(ufo[glyph_name].width) + ";\n")
+        inlined_otl.write("} dist;")
 
     ufo = Font.open(directory / "utn-font.ufo")
     ufo.info.familyName = "Draft UTN"
